db7b2213263c043c7f8df1bf10281e6929eb3f98
jack <giacomoradicchi@icloud.com>
2025-08-22 18:47:55 +0200

    aggiunta immagine arena

--- src/main/java/jroyale/controller/PrimaryController.java
+++ src/main/java/jroyale/controller/PrimaryController.java
@@ -1,13 +1,36 @@
 package jroyale.controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import jroyale.App;
 
-public class PrimaryController {
+public class PrimaryController implements Initializable{
+
+    @FXML
+    private ImageView arenaImageView;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Carica l'immagine dell'arena di Clash Royale
+        try {
+            Image arenaImage = new Image(getClass().getResourceAsStream("/jroyale/images/arena.png"));
+            arenaImageView.setImage(arenaImage);
+            arenaImageView.setFitWidth(arenaImage.getWidth()*0.6);
+            //arenaImageView.setFitHeight(arenaImage.getHeight()*0.6);
+            arenaImageView.setPreserveRatio(true); // Adatta esattamente alle dimensioni
+        } catch (Exception e) {
+            System.out.println("Errore nel caricamento dell'immagine dell'arena: " + e.getMessage());
+        }
     }
 }
