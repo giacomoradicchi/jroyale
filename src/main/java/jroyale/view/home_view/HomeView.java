5f120b6211025d94b43ccbfa71f31c86517bef57
jack <giacomoradicchi@icloud.com>
2026-04-22 03:08:09 +0200

    added logo

--- src/main/java/jroyale/view/home_view/HomeView.java
+++ src/main/java/jroyale/view/home_view/HomeView.java
@@ -2,6 +2,7 @@ package jroyale.view.home_view;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import jroyale.controller.GameEngine;
@@ -20,6 +21,12 @@ public class HomeView extends MainGUI implements IHomeView {
     private static final double NORMALIZED_DIFFICULTY_BOX_X = 0.5;
     private static final double NORMALIZED_DIFFICULTY_BOX_Y = NORMALIZED_PLAYBUTTON_Y + 2*NORMALIZED_PLAYBUTTON_TEXT_HEIGHT;
     private static final double NORMALIZED_DIFFICULTY_BOX_TEXT_HEIGHT = 0.04;
+    private static final double NORMALIZED_ARENA_SPRITE_X = 0.5;
+    private static final double NORMALIZED_ARENA_SPRITE_Y = 0.37;
+    private static final double NORMALIZED_ARENA_SPRITE_WIDTH = 0.5;
+    private static final double NORMALIZED_LOGO_X = 0.5;
+    private static final double NORMALIZED_LOGO_Y = 0.15;
+    private static final double NORMALIZED_LOGO_WIDTH = 0.67;
 
     private String difficulty;
 
@@ -27,8 +34,18 @@ public class HomeView extends MainGUI implements IHomeView {
 
     @Override
     public void init() {
-        this.fillScreenRoundedRect(getCanvasWidth()/2.0, getCanvasHeight()/2.0, getCanvasWidth(), getCanvasHeight(), 0, 0, 1, Color.BLUE);
+        renderScreenImage(new Image(this.getClass().getResourceAsStream("/jroyale/images/ui/home_background.jpg")), getCanvasWidth()/2, getCanvasHeight()/2, getCanvasWidth(), getCanvasHeight(), 1);
+        
+        Image image = new Image(this.getClass().getResourceAsStream("/jroyale/images/ui/ui_arena_sprite.png"));
+        double width = getCanvasWidth() * NORMALIZED_ARENA_SPRITE_WIDTH;
+        double height = image.getHeight() / image.getWidth() * width;
+        renderScreenImage(image, getCanvasWidth() * NORMALIZED_ARENA_SPRITE_X, getCanvasHeight() * NORMALIZED_ARENA_SPRITE_Y, width, height, 1);
 
+        image = new Image(this.getClass().getResourceAsStream("/jroyale/images/ui/jroyale_logo.png"));
+        width = getCanvasWidth() * NORMALIZED_LOGO_WIDTH;
+        height = image.getHeight() / image.getWidth() * width;
+        renderScreenImage(image, getCanvasWidth() * NORMALIZED_LOGO_X, getCanvasHeight() * NORMALIZED_LOGO_Y, width, height, 1);
+        
         Button playButton = new Button("Gioca Partita");
         FontManager fontManager = FontManager.getInstance();
         Font font = fontManager.getBoldFont(fontManager.getBoldFontSize(NORMALIZED_PLAYBUTTON_TEXT_HEIGHT * getCanvasHeight()));
@@ -77,10 +94,10 @@ public class HomeView extends MainGUI implements IHomeView {
         difficultyBox.setValue(difficulty); // default value
 
         difficultyBox.setStyle(
-            "-fx-background-color: #FFD700;" +
+            "-fx-background-color: #0095ff;" +
             "-fx-background-radius: 15;" +
             "-fx-border-radius: 15;" +
-            "-fx-border-color: #B8860B;" +
+            "-fx-border-color: #1595e4;" +
             "-fx-border-width: 2;" +
             "-fx-font-family: '" + fontManager.getBoldFont(NORMALIZED_DIFFICULTY_BOX_TEXT_HEIGHT).getFamily() + "';" +
             "-fx-font-size: 20px;" +
